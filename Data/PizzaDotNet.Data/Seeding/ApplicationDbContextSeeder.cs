c8095466e60e92795df79da1571b10c2ea5096ee
petar-staynov <petar.staynov@gmail.com>
2020-04-05 16:01:55 +0300

    Products seeder base

--- Data/PizzaDotNet.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Data/PizzaDotNet.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -28,6 +28,7 @@
                               new RolesSeeder(),
                               new SettingsSeeder(),
                               new CategoriesSeeder(),
+                              new ProductsSeeder(),
                           };
 
             foreach (var seeder in seeders)
---
+++ Data/PizzaDotNet.Data/Seeding/ProductsSeeder.cs
@@ -0,0 +1,28 @@
+﻿namespace PizzaDotNet.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using PizzaDotNet.Data.Models;
+
+    public class ProductsSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (!dbContext.Products.Any())
+            {
+                var products = new List<Product>
+                {
+                    new Product { Name = "A", Description = "A", Price = 0M, ImageUrl = "image.jpg", CategoryId = 1 },
+                };
+
+                foreach (Product product in products)
+                {
+                    await dbContext.Products.AddRangeAsync(products);
+                }
+            }
+        }
+    }
+}
