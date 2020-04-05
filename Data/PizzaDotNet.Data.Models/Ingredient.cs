5c046eb54fde86376ea40e45b696da036ba35bf4
petar-staynov <petar.staynov@gmail.com>
2020-04-05 16:02:36 +0300

    Ingredietns models

---
+++ Data/PizzaDotNet.Data.Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaDotNet.Data.Models
+{
+    using System.Collections.Generic;
+
+    using PizzaDotNet.Data.Common.Models;
+
+    public class Ingredient : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public virtual ICollection<ProductsIngredients> Products { get; set; }
+    }
+}
---
+++ Data/PizzaDotNet.Data.Models/ProductsIngredients.cs
@@ -0,0 +1,15 @@
+﻿namespace PizzaDotNet.Data.Models
+{
+    using PizzaDotNet.Data.Common.Models;
+
+    public class ProductsIngredients : BaseDeletableModel<int>
+    {
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int IngredientId { get; set; }
+
+        public virtual Ingredient Ingredient { get; set; }
+    }
+}
