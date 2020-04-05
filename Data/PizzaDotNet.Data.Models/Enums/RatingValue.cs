20d01ed5ccae3d8059c31c2fa8dd48e7ed30e3fb
petar-staynov <petar.staynov@gmail.com>
2020-04-05 16:02:43 +0300

    Ratings models

---
+++ Data/PizzaDotNet.Data.Models/Enums/RatingValue.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaDotNet.Data.Models.Enums
+{
+    public enum RatingValue
+    {
+        VeryBad = 0,
+        Bad = 1,
+        Ok = 2,
+        Good = 3,
+        Excellent = 4,
+    }
+}
---
+++ Data/PizzaDotNet.Data.Models/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaDotNet.Data.Models
+{
+    using PizzaDotNet.Data.Common.Models;
+    using PizzaDotNet.Data.Models.Enums;
+
+    public class Rating : BaseDeletableModel<int>
+    {
+        public RatingValue Value { get; set; }
+    }
+}
