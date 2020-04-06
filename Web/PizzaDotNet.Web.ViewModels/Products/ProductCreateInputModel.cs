4165d4bef1a66678ebfe89cc53e786dce2d82077
petar-staynov <petar.staynov@gmail.com>
2020-04-06 19:50:00 +0300

    Create product InputModel

---
+++ Web/PizzaDotNet.Web.ViewModels/Products/ProductCreateInputModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace PizzaDotNet.Web.ViewModels.Products
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    using PizzaDotNet.Web.ViewModels.Categories;
+
+    public class ProductCreateInputModel
+    {
+        [Required] public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required] public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryDropdownViewModel> Categories { get; set; }
+
+        public IEnumerable<SelectListItem> CategoriesListItems => this.Categories
+            .Select(x => new SelectListItem { Value = x.Id.ToString(), Text = x.Name });
+    }
+}
