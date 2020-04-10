e3055c633a2ee4cb798785246b0c65b85288ad47
petar-staynov <petar.staynov@gmail.com>
2020-04-10 13:21:08 +0300

    Product Image Migartion

---
+++ Data/PizzaDotNet.Data/Migrations/20200409163951_ProductImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaDotNet.Data.Migrations
+{
+    public partial class ProductImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageStorageName",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageStorageName",
+                table: "Products");
+        }
+    }
+}
