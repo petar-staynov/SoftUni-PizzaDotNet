78e085a5d4f1e5af4694651f7df7a5412230851e
petar-staynov <petar.staynov@gmail.com>
2020-04-10 19:08:46 +0300

    Image file custom validation attributes

---
+++ Data/PizzaDotNet.Data.Common/CustomValidationAttributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,40 @@
+﻿namespace PizzaDotNet.Data.Common.CustomValidationAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] extensions;
+
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            this.extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                var extension = Path.GetExtension(file.FileName);
+
+                if (!this.extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        //TODO Move error message to separate file
+        public string GetErrorMessage()
+        {
+            return $"This photo extension is not allowed!";
+        }
+    }
+}
---
+++ Data/PizzaDotNet.Data.Common/CustomValidationAttributes/MaxFileSizeAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace PizzaDotNet.Data.Common.CustomValidationAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int maxFileSize;
+
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            this.maxFileSize = maxFileSize;
+        }
+
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                if (file.Length > maxFileSize)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"Maximum allowed file size is { maxFileSize} bytes.";
+        }
+    }
+}
