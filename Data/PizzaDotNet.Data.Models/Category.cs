66a8fe60f49c61a98bd72c9368415105179b135f
petar-staynov <petar.staynov@gmail.com>
2020-04-21 01:29:47 +0300

    Database update - Images for Categories

--- Data/PizzaDotNet.Data.Models/Category.cs
+++ Data/PizzaDotNet.Data.Models/Category.cs
@@ -16,5 +16,9 @@
         public string Description { get; set; }
 
         public virtual ICollection<Product> Products { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ImageStorageName { get; set; }
     }
 }
---
+++ Data/PizzaDotNet.Data/Migrations/20200420222531_CategoriesImages.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaDotNet.Data.Migrations
+{
+    public partial class CategoriesImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageStorageName",
+                table: "Categories",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageStorageName",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Categories");
+        }
+    }
+}
