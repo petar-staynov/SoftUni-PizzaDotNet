35d07a7da6e2b92d500eae1ede82e512487ce315
petar-staynov <petar.staynov@gmail.com>
2020-04-25 22:06:37 +0300

    Cart page - Address Binding and Discount element

--- Web/PizzaDotNet.Web.ViewModels/Cart/CartViewModel.cs
+++ Web/PizzaDotNet.Web.ViewModels/Cart/CartViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace PizzaDotNet.Web.ViewModels.Cart
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaDotNet.Web.ViewModels.Cart
 {
     using System.Collections.Generic;
     using System.Linq;
@@ -12,8 +14,18 @@
 
         public ICollection<CartProductViewModel> Products { get; set; }
 
+        public int DiscountPercent { get; set; }
+
         public decimal? TotalPrice => this.Products.Select(p => p.Price * p.Quantity).Sum();
 
+        public decimal? DiscountPrice => this.TotalPrice * (1 - (this.DiscountPercent / 100));
+
         public CartAddressViewInputModel Address { get; set; }
+
+        [Display(Name = "Additional notes:")]
+        public string AdditionalNotes { get; set; }
+
+        [Display(Name = "Use this as my default address")]
+        public bool UseAddress { get; set; }
     }
 }
---
+++ Web/PizzaDotNet.Web/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿namespace PizzaDotNet.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class OrdersController : BaseController
+    {
+        public IActionResult PlaceOrder()
+        {
+            return this.Ok("ORDER PLACED");
+        }
+    }
+}
