d1566563a97819d4ff606486ae27ae1bf8b40ebe
petar-staynov <petar.staynov@gmail.com>
2020-04-27 00:30:00 +0300

    Normalized TempData message types

---
+++ PizzaDotNet.Common/AlertMessageTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaDotNet.Common
+{
+    public static class AlertMessageTypes
+    {
+        public const string Info = "primary";
+        public const string Success = "sucess";
+        public const string Warning = "warning";
+        public const string Error = "danger";
+    }
+}
