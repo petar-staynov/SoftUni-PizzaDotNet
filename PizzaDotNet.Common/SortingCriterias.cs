3081f0943122cc342c64c4dfa00517876e2ba70c
petar-staynov <petar.staynov@gmail.com>
2020-04-28 20:52:29 +0300

    Sorting Criterias Global - by username

--- PizzaDotNet.Common/SortingCriterias.cs
+++ PizzaDotNet.Common/SortingCriterias.cs
@@ -6,5 +6,7 @@
         public const string ORDER_PRICE_HIGHEST_TO_LOWEST = "price-highest-to-lowest";
         public const string ORDER_DATE_OLDEST_TO_NEWEST = "date-oldest-to-newest";
         public const string ORDER_DATE_NEWEST_TO_OLDEST = "date-newest-to-oldest";
+        public const string ORDER_USERNAME_ASCENDING = "user-name-ascending";
+        public const string ORDER_USENAME_DESCENDING = "user-name-descending";
     }
 }
