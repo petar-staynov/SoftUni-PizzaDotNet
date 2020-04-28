c81fa3c7d7fd5c20cc96ed26422bbb2622495909
petar-staynov <petar.staynov@gmail.com>
2020-04-28 20:52:45 +0300

    Admin Dashboard view model

--- Web/PizzaDotNet.Web.ViewModels/Administration/Dashboard/IndexViewModel.cs
+++ Web/PizzaDotNet.Web.ViewModels/Administration/Dashboard/IndexViewModel.cs
@@ -2,6 +2,14 @@
 {
     public class IndexViewModel
     {
-        public int SettingsCount { get; set; }
+        public int UsersCount { get; set; }
+
+        public int CategoriesCount { get; set; }
+
+        public int ProductsCount { get; set; }
+
+        public int OrdersCount { get; set; }
+
+        public decimal? OrdersTotalProfits { get; set; }
     }
 }
