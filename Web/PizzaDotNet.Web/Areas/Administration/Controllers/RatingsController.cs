0e7e08fbb1f46f756bf253ccdc230f8a9d02d435
petar-staynov <petar.staynov@gmail.com>
2020-04-28 20:51:31 +0300

    Administration basic pages and controllers

---
+++ Web/PizzaDotNet.Web/Areas/Administration/Controllers/RatingsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaDotNet.Web.Areas.Administration.Controllers
+{
+    public class RatingsController : AdministrationController
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/PizzaDotNet.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaDotNet.Web.Areas.Administration.Controllers
+{
+    public class UsersController : AdministrationController
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
